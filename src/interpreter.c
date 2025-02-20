36f314a5ae63009ae8e93fba588e7bb5b3ee4b91
DrParanoya <158059657+DrParanoya@users.noreply.github.com>
2025-02-20 10:42:30 +0100

    Code

---
+++ src/interpreter.c
@@ -0,0 +1,2 @@
+#define a ;break;case
+char c[1<<16],d[]="";unsigned short e,f,g;main(){for(;g<sizeof d;++g){switch(d[g]){a'+':c[e]++a'-':c[e]--a'>':e++a'<':e--a'.':putchar(c[e])a',':c[e]=getchar()a'[':f=++g;for(;c[e]!=0;main()){g=f;}a']':return;}}}
